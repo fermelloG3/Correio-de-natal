982b98e78ef60712ebb471150aeeda33c77ec0d3
fernando <ferdemello28@gmail.com>
2024-12-15 11:42:32 -0300

    cambio en sabe message

--- api/save-message.js
+++ api/save-message.js
@@ -1,7 +1,25 @@
-const express = require('express');
-const router = express.Router();
+const allowCors = (fn) => async (req, res) => {
+  const allowedOrigins = ['https://correio-de-natal.vercel.app', 'http://localhost:3000']; // Dominios permitidos
+  const origin = req.headers.origin;
 
-router.post('/', async (req, res) => {
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+
+  if (req.method === 'OPTIONS') {
+    res.status(200).end(); // Responder directamente a las solicitudes preflight
+    return;
+  }
+  return await fn(req, res);
+};
+
+const handler = async (req, res) => {
   try {
     console.log('Datos recibidos:', req.body);
     const { senderHotel, senderName, recipientHotel, recipientName, customMessage } = req.body;
@@ -28,6 +46,6 @@ router.post('/', async (req, res) => {
   } catch (err) {
     res.status(500).json({ error: 'Error al guardar el mensaje', details: err.message });
   }
-});
+};
 
-module.exports = router;
+module.exports = allowCors(handler);
